a47be45ff156d73a568b002998b253f2dc4fa6eb
Peat <peachaya.s4403@gmail.com>
2021-08-29 11:19:17 -0400

    Phone added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Peachaya Suwanteamee");
         System.out.println("Age: 32");
+        System.out.println("Phone : 000-000-0000");
+
 
     }
 }
